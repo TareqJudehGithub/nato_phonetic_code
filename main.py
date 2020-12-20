d2425c3a8bfe476c86de41799050230c63c4a569
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-20 21:46:57 +0200

    Implemented Exceptions, to the project, where strings-only inputs are accepted. Any other input data will not be accepted, and will cause the system to repeat the question for the user.

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 import pandas
 
 # TODO 1. Create a dictionary:
-
 phonetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
+
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# User input:
+
+bad_word = True
+while bad_word:
+    word = input("Enter a word: ").upper()
+
+    try:
+        code_list = [phonetic_dict[letter] for letter in word]
+
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        bad_word = True
+
+    else:
+        bad_word = False
+        # codes in string:
+        str_code = ", ".join(code_list)
+        print(str_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Another solution using DataFrame:
 # phonetic_dict= {letter: code for (letter, code) in phonetic_data.items()}
@@ -12,10 +45,3 @@ phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows
 # nato_dict = {}
 # for (index, row) in phonetic_data_frame.iterrows():
 #     nato_dict[row.letter] = row.code
-
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# User input:
-word = input("Enter a word: ").upper()
-code_list = [phonetic_dict[letter] for letter in word]
-print(code_list)
-
